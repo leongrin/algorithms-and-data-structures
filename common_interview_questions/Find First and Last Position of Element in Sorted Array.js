d93812fbd2c97b5ee1a1c35bfedaecfdab2a15c1
leongrin <ceo@brmed.com>
2022-03-30 18:56:46 -0300

    update

--- common_interview_questions/Find First and Last Position of Element in Sorted Array.js
+++ common_interview_questions/Find First and Last Position of Element in Sorted Array.js
@@ -5,38 +5,47 @@
 //
 // You must write an algorithm with O(log n) runtime complexity.
 
-const searchRange = (nums, target) => {
-    if (!nums.length) return [-1,-1];
+const searchRange = function (nums, target) {
 
-    let start = 0;
-    let end = nums.length - 1;
-    let middle = Math.floor((start + end) / 2);
-    console.log(`middle => ${middle}, nums[middle] => ${nums[middle]}, target => ${target}`)
+    const binarySearch = (nums, left, right) => {
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) return mid;
+            else if (target > nums[mid]) left = mid + 1;
+            else right = mid - 1;
+        }
 
-    while(nums[middle] !== target && start <= end) {
-        if(target < nums[middle]) end = middle - 1;
-        else start = middle + 1;
-        middle = Math.floor((start + end) / 2);
+        return null;
+    };
+
+    if (nums.length < 1) return [-1, -1];
+
+    const firstPos = binarySearch(nums, 0, nums.length - 1);
+
+    if (firstPos === null) return [-1, -1];
+
+    let endPos = firstPos,
+        startPos = firstPos,
+        temp1,
+        temp2;
+
+    while (startPos !== null) {
+        temp1 = startPos;
+        startPos = binarySearch(nums, 0, startPos - 1);
     }
+    startPos = temp1;
 
-    if (nums[middle] !== target) return [-1,-1];
-
-    start = middle;
-    end = middle;
-
-    console.log(`middle => ${middle}`)
-
-    while (nums[start] === target && nums[end] === target) {
-        if (nums[start - 1] === target) start--;
-        if (nums[end + 1] === target) end++;
-        if (nums[start - 1] !== target && nums[end + 1] !== target) break;
+    while (endPos !== null) {
+        temp2 = endPos;
+        endPos = binarySearch(nums, endPos + 1, nums.length - 1);
     }
+    endPos = temp2;
 
-    return [start, end];
+    return [startPos, endPos];
 };
 
 
-console.log(searchRange([0,0,0,1,2,3], 0));
+console.log(searchRange([5,7,7,8,8,10], 8));
 
 
 // Example 1:
